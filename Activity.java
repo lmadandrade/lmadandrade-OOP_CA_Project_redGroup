1da115328bbfaa0d64c46d59ec962f83a2e1182e
Lucas Andrade <94678369+lmadandrade@users.noreply.github.com>
2024-10-31 19:59:01 +0000

    Create Activity.java

---
+++ Activity.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package healthappredgroup;
+
+/**
+ *
+ * @author lucasandrade
+ */
+public class Activity {
+    private String type;
+    private double duration;
+    
+    public void addActivity (String type, double duration){
+    // TO Do
+    }
+    
+    public void viewActivity(){
+    // TODO
+    }
+    
+}
